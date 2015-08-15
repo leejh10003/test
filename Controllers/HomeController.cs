b2e0f381726d106efd6a899214a0c12fad9ae55e
leejunhyuk <leejh10003@gmail.com>
2015-08-15 19:09:04 +0900

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+using MvcSample.Web.Models;
+
+namespace MvcSample.Web
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(CreateUser());
+        }
+
+        public User CreateUser()
+        {
+            User user = new User()
+            {
+                Name = "이준혁",
+                Address = "서울특별시 동대문구 용두동 793 BYC 청량리 오피스텔"
+            };
+
+            return user;
+        }
+    }
+}
